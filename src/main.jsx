573d5bc60eaf4c5da2828469e1b6f55844ba64c2
NikhilSharma206 <nsharma4_be23@thapar.edu>
2025-11-26 20:12:05 +0530

    deploy

--- src/main.jsx
+++ src/main.jsx
@@ -11,3 +11,6 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     </BrowserRouter>
   </React.StrictMode>
 );
+import axios from "axios";
+axios.defaults.baseURL =
+  import.meta.env.VITE_API_URL || "http://localhost:5000";
